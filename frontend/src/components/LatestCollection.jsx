79387147479fd0c7277546c1097cec3fcb280db7
vivek-310 <vivekvardhan0310@gmail.com>
2025-01-22 21:30:35 +0530

    navbar update

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import {ShopContext} from '../context/ShopContext'
+const LatestCollection = () => {
+
+const {products}=useContext(ShopContext)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default LatestCollection
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,6 +44,20 @@ const Navbar = () => {
             </Link>
             <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 sm:hidden cursor-pointer' alt=''></img>
         </div>
+        {/* sidebar menu*/}
+        <div className={`absolute top-0 bottom-0 right-0 overflow-hidden bg-white transition-all ${visible?'w-full':'w-0'} `}>
+                <div className='flex flex-col text-gray-600'>
+                    <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+                        <img className='h-5 rotate-180 cursor-pointer' src={assets.dropdown_icon} alt=''/>
+                        <p className='cursor-pointer'>Back</p>
+                    </div>
+                    <NavLink onClick={()=>{setVisible(false)}} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={()=>{setVisible(false)}} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={()=>{setVisible(false)}} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={()=>{setVisible(false)}} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+
+                </div>
+        </div>
     </div>
   )
 }
