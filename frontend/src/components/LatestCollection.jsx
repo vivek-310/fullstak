5d506bfc4507a6496926c6e2ccf88130c3a8947f
vivek-310 <vivekvardhan0310@gmail.com>
2025-01-22 23:17:24 +0530

    hero update

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,32 @@
-import React, { useContext } from 'react'
-import {ShopContext} from '../context/ShopContext'
-const LatestCollection = () => {
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import Productitem from './Productitem';
 
-const {products}=useContext(ShopContext)
+const LatestCollection = () => {
+    const {products}=useContext(ShopContext);
+    const[latestProducts,setlatestProducts]=useState([]);
+
+    useEffect(
+        ()=>{
+            setlatestProducts(products.slice(0,10));
+        },[]
+    )
 
   return (
-    <div>
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl '>
+            <Title text1={'LATEST'} text2={'COLLECTION'} />
+
+        </div>
+            {/* products*/}
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {
+                    latestProducts.map((item,index)=>(
+                        <Productitem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                    ))
+                }
+            </div>
 
     </div>
   )
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,20 +1,21 @@
 import { createContext } from "react";
 import { products } from "../assets/assets";
 
-export const ShopContext= createContext();
+
+export const ShopContext = createContext();
 
 const ShopContextProvider=(props)=>{
     const currency='$';
     const delivery_fee=10;
 
     const value={
-        products,currency,delivery_fee
+       products,currency,delivery_fee
     }
 
     return(
-        <ShopContextProvider value={value}>
-            {props.childern}
-        </ShopContextProvider>
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
     )
 }
 
