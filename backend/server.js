96f83aeed4a4d90831dff9d9161de1a3ade619ff
vivek-310 <vivekvardhan0310@gmail.com>
2025-02-27 22:17:07 +0530

    backend intial

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("hello")
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+
+const app=express();
+const port=process.env.PORT || 4000
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get('/',(req,res)=>{
+    res.send("API is working");
+})
+
+app.listen(port,()=>console.log("app is listening at port:"+port));
